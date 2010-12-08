94666496b241f2def4832c36e92a0440f95b334b
Bob Frank <bob@bobfrank.org>
2010-12-07 21:11:05 -0500

    misc parse tree enhancements.. more to come

--- blt.py
+++ blt.py
@@ -3,22 +3,33 @@ reserved = {
     'const':    'CONST',
     'class':    'CLASS',
     'template': 'TEMPLATE',
-    'operator': 'OPERATOR'
+    'operator': 'OPERATOR',
+    'elif':     'ELIF',
+    'else':     'ELSE',
+    'if':       'IF',
+    #'float':    'FLOAT',
+    #'char':     'CHAR',
+    #'double':   'DOUBLE',
+    #'int':      'INT',
     }
 tokens = ['COMMENT',
-    'NAME','FLOAT', 'POINTS', 'BITAND',
+    'ID','POINTS', 'BITAND',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN', 'INT', 'SCOPE','OPEN',
+    'LPAREN','RPAREN', 'SCOPE','OPEN',
     'CLOSE', 'DOT', 'COMMA', 'EXTERNCODE',
-    'STRING', 'COLON', 'SEMICOLON', 'DOLLAR',
-    'LBRACKET','RBRACKET'
+    'COLON', 'SEMICOLON', 'DOLLAR',
+    'LBRACKET','RBRACKET', 'GT',
+    'INTCONST', 'FLOATCONST', 'STRINGCONST'
     ] + list(reserved.values())
 
 # Tokens
 
 t_COMMENT = r'//[^\n\r]*'
-t_STRING  = r'".*?"'
+t_STRINGCONST  = r'".*?"'
+t_FLOATCONST   = r'\d+\.\d+'
+t_INTCONST     = r'\d+'
 t_POINTS  = r'=>'
+t_GT      = r'>'
 t_OPEN    = r'{'
 t_CLOSE   = r'}'
 t_BITAND  = r'&'
@@ -34,8 +45,6 @@ t_LBRACKET= r'\['
 t_RBRACKET= r'\]'
 t_SCOPE   = r'::'
 t_COLON   = r':'
-t_FLOAT   = r'\d+\.\d+'
-t_INT     = r'\d+'
 t_DOT     = r'\.'
 t_COMMA   = r','
 t_EXTERNCODE= r'{{.*?}}'
@@ -43,9 +52,9 @@ t_SEMICOLON = r';'
 # Ignored characters
 t_ignore = " \t"
 
-def t_NAME(t):
+def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value, 'NAME')
+    t.type = reserved.get(t.value, 'ID')
     return t
 
 def t_newline(t):
@@ -64,9 +73,6 @@ def p_error(t):
 
 
 # Parsing rules
-FUNDEF,FUNCTION,EXTFUNCTION,CLASS,TEMPLATE_CLASS,ARG,FNAME,TYPE,EXTTYPE,REF,PTR,\
-    CONST,OBJ,SUBOBJ,DEREF,LOCAT,TEMPLATE_ARG,TEMPLATE_ARGSPEC,TEMPLATED_TYPE,\
-    CONSTRUCT, OPERATOR = range(21)
 
 precedence = ()
 
@@ -95,25 +101,25 @@ def p_segment_cls(t):
 
 def p_operator_bracket(t):
     'operator : OPERATOR LBRACKET RBRACKET'
-    t[0] = [OPERATOR, '[]']
+    t[0] = ['OPERATOR', '[]']
 
 def p_fnname_id(t):
-    'fnname : NAME'
-    t[0] = [FNAME, None, t[1]]
+    'fnname : ID'
+    t[0] = ['FNAME', None, t[1]]
 def p_fnname_operator(t):
     'fnname : operator'
-    t[0] = [FNAME, None, t[1]]
+    t[0] = ['FNAME', None, t[1]]
 def p_fnname_class(t):
-    'fnname : NAME SCOPE NAME'
-    t[0] = [FNAME, t[1], t[3]]
+    'fnname : ID SCOPE ID'
+    t[0] = ['FNAME', t[1], t[3]]
 
 
 def p_templatearg_type(t):
-    'templatearg : NAME EQUALS NAME'
-    t[0] = [TEMPLATE_ARG, t[1], t[3]]
+    'templatearg : ID EQUALS ID'
+    t[0] = ['TEMPLATE_ARG', t[1], t[3]]
 def p_templatearg_bdata(t):
-    'templatearg : NAME EQUALS bdata'
-    t[0] = [TEMPLATE_ARG, t[1], t[3]]
+    'templatearg : ID EQUALS constant'
+    t[0] = ['TEMPLATE_ARG', t[1], t[3]]
 
 def p_templateargs_one(t):
     'templateargs : templatearg'
@@ -127,18 +133,18 @@ def p_templateargs_more(t):
 
 #TODO figure out what to use for string's... maybe force const char*?
 def p_templargspec_type(t):
-    'templargspec : NAME NAME EQUALS NAME'
-    t[0] = [TEMPLATE_ARGSPEC, t[1], t[3]]
+    'templargspec : ID ID EQUALS ID'
+    t[0] = ['TEMPLATE_ARGSPEC', t[1], t[3]]
     if t[1] != 'typename':
         raise SyntaxError
 def p_templargspec_bdata(t):
-    'templargspec : NAME NAME EQUALS bdata'
-    t[0] = [TEMPLATE_ARGSPEC, t[1], t[2], t[4]]
+    'templargspec : ID ID EQUALS constant'
+    t[0] = ['TEMPLATE_ARGSPEC', t[1], t[2], t[4]]
     if t[1] not in ['int','float','string']:
         raise SyntaxError
 def p_templargspec_nodefault(t):
-    'templargspec : NAME NAME'
-    t[0] = [TEMPLATE_ARGSPEC, t[1], t[2], None]
+    'templargspec : ID ID'
+    t[0] = ['TEMPLATE_ARGSPEC', t[1], t[2], None]
     if t[1] not in ['typename','int','float','string']:
         raise SyntaxError
 
@@ -153,89 +159,88 @@ def p_templargspecs_more(t):
 
 
 def p_dname_name(t):
-    'dname : NAME'
-    t[0] = [OBJ, t[1]]
+    'dname : ID'
+    t[0] = t[1]
 def p_dname_more(t):
-    'dname : dname DOT NAME'
-    t[0] = [SUBOBJ, t[1], t[3]]
+    'dname : dname DOT ID'
+    t[0] = ['SUBUTYPE', t[1], t[3]]
 
 
 def p_etype_plain(t):
     'etype : dname'
-    t[0] = [TYPE, t[1]]
+    t[0] = ['TYPE', t[1]]
 def p_etype_templated(t):
     'etype : dname LPAREN templateargs RPAREN'
-    t[0] = [TEMPLATED_TYPE, t[1], t[3]]
+    t[0] = ['TEMPLATED_TYPE', t[1], t[3]]
 def p_etype_external(t):
-    'etype : DOLLAR NAME'
-    t[0] = [EXTTYPE, t[1]]
+    'etype : DOLLAR ID'
+    t[0] = ['EXTTYPE', t[2]]
 def p_rtype_plain(t):
     'rtype : etype'
     t[0] = t[1]
 def p_ptype_ref(t):
     'rtype : etype BITAND'
-    t[0] = [REF, t[1]]
+    t[0] = ['REF', t[1]]
 def p_ptype_plain(t):
     'ptype : rtype'
     t[0] = t[1]
 def p_ptype_ptr(t):
     'ptype : rtype TIMES'
-    t[0] = [PTR, t[1]]
+    t[0] = ['PTR', t[1]]
 def p_ctype_plain(t):
     'ctype : ptype'
     t[0] = t[1]
 def p_ctype_const(t):
     'ctype : CONST ptype'
-    t[0] = [CONST, t[2]] #TODO make this like c++ const
+    t[0] = ['CONST', t[2]] #TODO make this like c++ const
 
 
 def p_constructor_args(t):
     'constructor : dname LPAREN args RPAREN'
-    t[0] = [CONSTRUCT, t[1], t[3]]
+    t[0] = ['CONSTRUCT', t[1], t[3]]
 def p_constructor_void(t):
     'constructor : dname LPAREN RPAREN'
-    t[0] = [CONSTRUCT, t[1], None]
+    t[0] = ['CONSTRUCT', t[1], None]
 def p_constructor_tvoid(t):
     'constructor : dname LPAREN templateargs RPAREN LPAREN RPAREN'
     mytype = [TEMPLATED_TYPE, t[1], t[3]]
-    t[0] = [CONSTRUCT, mytype, None]
+    t[0] = ['CONSTRUCT', mytype, None]
 def p_constructor_targs(t):
     'constructor : dname LPAREN templateargs RPAREN LPAREN args RPAREN'
-    t[0] = [CONSTRUCT, mytype, t[6]]
+    t[0] = ['CONSTRUCT', mytype, t[6]]
 
-
-def p_bdata_int(t):
-    'bdata : INT'
+def p_constant_int(t):
+    'constant : INTCONST'
     t[0] = int(t[1])
-def p_bdata_float(t):
-    'bdata : FLOAT'
+def p_constant_float(t):
+    'constant : FLOATCONST'
     t[0] = float(t[1])
-def p_bdata_string(t):
-    'bdata : STRING'
+def p_constant_string(t):
+    'constant : STRINGCONST'
     t[0] = t[1][1:-1]
-def p_data_bdata(t):
-    'data : bdata'
+
+def p_data_constant(t):
+    'data : constant'
     t[0] = t[1]
 def p_data_name(t):
     'data : dname'
     t[0] = t[1]
 def p_data_ptr(t):
     'data : BITAND dname'
-    t[0] = [LOCAT, t[1]]
+    t[0] = ['LOCAT', t[1]]
 def p_data_deref(t):
     'data : TIMES dname'
-    t[0] = [DEREF, t[1]]
+    t[0] = ['DEREF', t[1]]
 def p_data_construct(t):
     'data : constructor'
     t[0] = t[1]
 
 def p_arg_required(t):
-    'arg : ctype NAME'
-    t[0] = [ARG, t[1], t[2], None]
+    'arg : ctype ID'
+    t[0] = ['ARG', t[1], t[2], None]
 def p_arg_optional(t):
-    'arg : ctype NAME EQUALS data'
-    t[0] = [ARG, t[1], t[2], t[4]]
-
+    'arg : ctype ID EQUALS data'
+    t[0] = ['ARG', t[1], t[2], t[4]]
 
 def p_args_one(t):
     'args : arg'
@@ -247,9 +252,81 @@ def p_args_more(t):
     t[0] = tmp
 
 
+def p_callarg(t):
+    'callarg : ID EQUALS data'
+    t[0] = ['CALLARG', t[1], t[3]]
+
+def p_callargs_one(t):
+    'callargs : callarg'
+    t[0] = [t[1]]
+def p_callargs_more(t):
+    'callargs : callargs COMMA callarg'
+    tmp = t[1]
+    tmp.append(t[3])
+    t[0] = tmp
+
+
+def p_compare(t):
+    'compare : rtype GT data' #TODO actually write this up.. just want it to compile
+    t[0] = ['GT', t[1], t[3]]
+
+def p_bareif(t):
+    'bareif : IF LPAREN compare RPAREN block'
+    t[0] = ['IF', t[3], t[5]]
+
+def p_elif(t):
+    'elif : ELIF LPAREN compare RPAREN block'
+    t[0] = ['ELIF', t[3], t[5]]
+
+def p_else(t):
+    'else : ELSE block'
+    t[0] = ['ELSE', t[2]]
+
+def p_elifs_one(t):
+    'elifs : elif'
+    t[0] = [t[1]]
+def p_elifs_many(t):
+    'elifs : elifs elif'
+    tmp = t[1]
+    tmp.append(t[2])
+    t[0] = tmp
+
+def p_elses_with(t):
+    'elses : elifs else'
+    t[0] = [t[1],t[2]]
+def p_elses_just(t):
+    'elses : else'
+    t[0] = [None,t[1]]
+
+def p_ifstatement_if(t):
+    'ifstatement : bareif'
+    t[0] = t[1]
+def p_ifstatement_elses(t):
+    'ifstatement : bareif elses'
+    t[0] = ['IFESLE',t[0], t[1]]
+
+def p_oblock_if(t):
+    'oblock : ifstatement'
+    t[0] = t[1]
+def p_oblock_fncall(t):
+    'oblock : funcall SEMICOLON'
+    t[0] = t[1]
+
+def p_blocks_one(t):
+    'blocks : oblock'
+    t[0] = t[1]
+def p_blocks_more(t):
+    'blocks : block oblock'
+    tmp = t[1]
+    tmp.append(t[2])
+    t[0] = tmp
+
+def p_block_empty(t):
+    'block : OPEN CLOSE'
+    t[0] = []
 def p_block(t):
-    'block : '
-    t[0] = ''
+    'block : OPEN blocks CLOSE'
+    t[0] = t[1]
 
 
 def p_argret_void(t):
@@ -267,15 +344,25 @@ def p_argret_return(t):
 
 
 def p_function_args(t):
-    'function : fnname argret OPEN block CLOSE'
-    t[0] = [FUNCTION, t[1], t[2], t[4]]
+    'function : fnname argret block'
+    t[0] = ['FUNCTION', t[1], t[2], t[3]]
+def p_function_init(t):
+    'function : fnname argret COLON callargs block'
+    t[0] = ['INITFUNCTION', t[1], t[2], t[4], t[5]]
 def p_function_extern(t):
-    'function : fnname argret EQUALS EXTERN STRING argret EXTERNCODE SEMICOLON'
-    t[0] = [EXTFUNCTION, t[1], t[2], t[5][1:-1], t[6], t[7][2:-2]]
+    'function : fnname argret EQUALS EXTERN STRINGCONST argret EXTERNCODE SEMICOLON'
+    t[0] = ['EXTFUNCTION', t[1], t[2], t[5][1:-1], t[6], t[7][2:-2]]
+
+def p_funcall_args(t):
+    'funcall : fnname LPAREN callargs RPAREN'
+    t[0] = ['FUNCALL', t[1], t[3]]
+def p_funcall_void(t):
+    'funcall : fnname LPAREN RPAREN'
+    t[0] = ['FUNCALL', t[1], None]
 
 def p_fundef(t):
     'fundef : fnname argret SEMICOLON'
-    t[0] = [FUNDEF, t[1], t[2]]
+    t[0] = ['FUNDEF', t[1], t[2]]
 
 def p_fundefs_one(t):
     'fundefs : fundef'
@@ -287,12 +374,12 @@ def p_fundefs_many(t):
     t[0] = tmp
 
 def p_class(t):
-    'class : CLASS NAME OPEN fundefs CLOSE'
-    t[0] = [CLASS, t[2], t[4]]
+    'class : CLASS ID OPEN fundefs CLOSE'
+    t[0] = ['CLASS', t[2], t[4]]
 
 def p_tclass_template(t):
     'tclass : TEMPLATE LPAREN templargspecs RPAREN class'
-    t[0] = [TEMPLATE_CLASS, t[3], t[5]]
+    t[0] = ['TEMPLATE_CLASS', t[3], t[5]]
 def p_tclass_plain(t):
     'tclass : class'
     t[0] = t[1]
